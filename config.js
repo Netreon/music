d14bd530d81f9d8ff712538fd42b673425e8c58e
Wtfberk1 <142720397+Wtfberk1@users.noreply.github.com>
2023-08-22 12:32:44 +0300

    Update config.js
    Müzik v14

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: "Botunuzun Tokenini Giriniz",
-    id: "Botunuzun ID'sini Giriniz"
+    token: "Botunuzun Tokenini Giriniz",MTE0MzQ3NzA3NjM4MTQwNTE5NA.GJayHy.1crS30xdmLDCla4-JwEsfDZEQVGnS2DZR_jQvk
+    id: "Botunuzun ID'sini Giriniz"1143477076381405194
 }
